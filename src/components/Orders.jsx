7458cc5afc0cb48178924622d529c9b5e0b2a1ed
venkatreddy2001 <venkatreddymeka02@gmail.com>
2025-03-04 22:11:06 +0000

    Changes 1

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BASE_URL } from '../config';
 
 const Orders = () => {
@@ -10,7 +10,16 @@ const Orders = () => {
    * 2. Using the `useEffect` hook, update the existing `orders` state object when `fetchOrders` is complete
    **/ 
 
-
+  useEffect(()=>{
+    fetchOrders();
+  },[])
+  const fetchOrders = () => {
+    fetch(`${BASE_URL}/orders`)  
+    .then((res) => res.json())
+    .then((data) => {
+      setOrders(data);
+    })
+  }
   return (
     <div className="center mw7 ba mv4">
       <div className="bg-white pa3 mb3">
